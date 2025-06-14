ca2ba3364ba844b21e0034e780200f535ba68a18
Emil <202892870+emiljns@users.noreply.github.com>
2025-06-14 10:28:58 +0200

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -5,3 +5,17 @@ export interface Metadata {
   excerpt: string;
   keywords: string[];
 }
+
+export interface PageProps {
+  params: {
+    entry: string;
+  };
+}
+
+export interface Metadata {
+  name: string;
+  slug: string;
+  date: Date;
+  excerpt: string;
+  keywords: string[];
+}
